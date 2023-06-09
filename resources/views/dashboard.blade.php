52c344df0c03ab74b99bbf43da65cfb6558deb46
Ashish Patel <45224160+Ashu1992patel@users.noreply.github.com>
2023-06-10 04:42:02 +0530

    envelope icon

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -83,7 +83,7 @@
                     <div class="col-12 col-sm-6 col-md-3">
                         <div class="info-box">
                             <span class="info-box-icon bg-success elevation-1">
-                                <i class="fas fa-list"></i>
+                                <i class="fas fa-envelope"></i>
                             </span>
 
                             <div class="info-box-content">
