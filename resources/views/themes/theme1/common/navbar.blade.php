15a32bb6d25f30a879ec93e6fb912d0a47003f92
Ashish Patel <45224160+Ashu1992patel@users.noreply.github.com>
2023-06-10 17:29:11 +0530

    Change in mobile navbar logo

--- resources/views/themes/theme1/common/navbar.blade.php
+++ resources/views/themes/theme1/common/navbar.blade.php
@@ -2,10 +2,17 @@
 <nav class="navbar navbar-expand-lg bg-primary navbar-dark shadow-sm py-3 py-lg-0 px-3 px-lg-5">
     <a href="index.html" class="navbar-brand d-flex d-lg-none">
         <h1 class="m-0 display-4 text-secondary">
-            <span class="text-white">
-                Farm
-            </span>
-            Fresh
+
+            @if (session()->has('settings'))
+                <img src="{{ url(session('settings')->logo ?? 'logo.png') }}" alt="Logo" width="45px">
+            @else
+                <span class="text-white">
+                    Ghunsaur
+                    {{-- Ghunsaur Farmer --}}
+                </span>
+                Farmer
+                {{-- Producer Company Ltd. --}}
+            @endif
         </h1>
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
@@ -13,16 +20,21 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav mx-auto py-0">
-            <a href="{{ url('/') }}" class="nav-item nav-link  {{ request()->is('/') ? 'active' : '' }}">Home</a>
-            <a href="{{ url('about') }}"
-                class="nav-item nav-link {{ request()->is('about') ? 'active' : '' }}">About</a>
-            <a href="{{ url('services') }}"
-                class="nav-item nav-link {{ request()->is('services') ? 'active' : '' }}">Service</a>
-            <a href="{{ url('products') }}"
-                class="nav-item nav-link {{ request()->is('products') ? 'active' : '' }}">Product</a>
-            <a href="{{ url('blogs') }}"
-                class="nav-item nav-link {{ request()->is('blogs') ? 'active' : '' }}">Blogs</a>
-
+            <a href="{{ url('/') }}" class="nav-item nav-link  {{ request()->is('/') ? 'active' : '' }}">
+                Home
+            </a>
+            <a href="{{ url('about') }}" class="nav-item nav-link {{ request()->is('about') ? 'active' : '' }}">
+                About
+            </a>
+            <a href="{{ url('services') }}" class="nav-item nav-link {{ request()->is('services') ? 'active' : '' }}">
+                Service
+            </a>
+            <a href="{{ url('products') }}" class="nav-item nav-link {{ request()->is('products') ? 'active' : '' }}">
+                Product
+            </a>
+            <a href="{{ url('blogs') }}" class="nav-item nav-link {{ request()->is('blogs') ? 'active' : '' }}">
+                Blogs
+            </a>
             {{-- <div class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                 <div class="dropdown-menu m-0">
@@ -34,8 +46,9 @@
                 </div>
             </div> --}}
 
-            <a href="{{ url('contact') }}"
-                class="nav-item nav-link {{ request()->is('contact') ? 'active' : '' }}">Contact</a>
+            <a href="{{ url('contact') }}" class="nav-item nav-link {{ request()->is('contact') ? 'active' : '' }}">
+                Contact
+            </a>
         </div>
     </div>
 </nav>
